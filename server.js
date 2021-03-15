3edb7d9f00088cf97d7d072ff50b6782aa7bd972
Matthew John <MattJ900@outlook.com>
2021-03-15 16:53:34 -0700

    Added html routes and server js roar

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-
+app.use(express.urlencoded({ extended: true })); // url coding helps not use incorrect symbols
+app.use(express.json()); // will automatically parse json and use in routes
+app.use(express.static("public")) // send browsser into public folder as the front end
+app.use(require("./routes/api-routes"))
+app.use(require("./routes/html-routes"))
 
 
 
