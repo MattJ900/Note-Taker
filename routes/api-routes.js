0596fd932df5f4ddb2756a4096be4e58d56a2b35
Matthew John <MattJ900@outlook.com>
2021-03-15 17:27:08 -0700

    Added API Routes rOAR

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,27 @@
 const router = require("express").Router();
+const fs = require("fs");
 
+router.get("/api/notes",(req,res)=>{
+fs.readFile('./db/db.json', 'utf8', (error, data) =>{
+if (error) throw error 
+const notes = JSON.parse(data);
+res.json(notes);
+});
+  
+})
 
-
+router.post("/api/notes",(req,res)=>{
+fs.readFile('./db/db.json', 'utf8', (error, data) =>{
+if (error) throw error 
+const notes = JSON.parse(data);
+const newNote = req.body;
+notes .push(newNote)
+fs.writeFile('./db/db.json', JSON.stringify(notes), (err) =>{
+if (error) throw error 
+res.json(newNote)
+});
+});    
+})
 
 
 
